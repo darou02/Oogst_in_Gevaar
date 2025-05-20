3a14328806472dd0a73e5e4faea608d78d23f26b
Dario Rousseaux <dario.rousseaux@gmail.com>
2025-05-20 17:34:45 +0200

    modified messages

--- js/transitions.js
+++ js/transitions.js
@@ -53,41 +53,41 @@ class Transition {
 
         // Special messages for chemical and CRISPR choices
         if (this.chosenIngredients.includes('I_Pesticide')) {
-            this.message = 'Chemische pesticiden zullen ook nuttige insecten zoals bijen en vlinders schaden! Biologische oplossingen zijn vaak beter!';
+            this.message = 'Sommige pesticiden doden niet alleen schadelijke insecten, maar ook nuttige zoals bijen en vlinders. Biologische oplossingen zijn beter, omdat ze de goede insecten niet beschadigen en daardoor de natuur beschermen';
         } else if (this.chosenIngredients.includes('D_CRISPR')) {
-            this.message = 'Super! Dankzij CRISPR kunnen we planten verbeteren zonder schadelijke stoffen te gebruiken. Hierdoor kunnen we de planten beter tegen droogte beschermen!';
+            this.message = 'Super!Dankzij CRISPR kunnen we het DNA van planten heel precies aanpassen. Hierdoor kunnen we planten maken die zich beter aanpassen aan droogte, zonder schadelijke stoffen te gebruiken!';
         } else if (this.chosenIngredients.includes('D_Chemical')) {
-            this.message = 'Dit middel helpt de grond om langer water vast te houden, maar door die chemische stoffen raakt jouw bodem wel uitgeput en ziek, wat niet goed is voor planten en dieren.';
+            this.message = 'Dit middel helpt de grond om langer water vast te houden, maar door die chemische stoffen raakt jouw bodem wel uitgeput en ziek, wat schadelijk is voor planten en dieren.';
         } else if (this.chosenIngredients.includes('D_Fertilizer')) {
-            this.message = 'Mest is goed voor planten, maar te veel ervan zal jouw grond en water vervuilen...';
+            this.message = 'Mest helpt planten groeien, omdat het vol voeding zit. Maar als je te veel mest gebruikt, spoelen de extra stoffen weg naar de grond en het water. Dat kan rivieren en meren vervuilen en het leven daar verstoren.';
         } else if (this.chosenIngredients.includes('D_Microbial')) {
-            this.message = 'Dit is een goede oplossing! Sommige bodembacteriën kunnen planten via de wortelen helpen om in droogte beter te groeien, helemaal natuurlijk!';
+            this.message = 'Dit is een slimme en natuurlijke oplossing! Sommige bacteriën die in de bodem leven, helpen planten via hun wortels om beter te groeien, zelfs als het droog is door bijvoorbeeld planten meer water te laten opnemen. Zo hoeven we geen chemische producten te gebruiken!';
         } else if (this.chosenIngredients.includes('D_Wild_Relatives')) {
-            this.message = 'Sommige familieleden van jouw planten kunnen van nature goed tegen droogte door bijvoorbeeld hun grote wortels, maar dit zijn niet degene die wij hier groeien om op te eten.';
+            this.message = 'Sommige plantensoorten uit dezelfde familie van jouw planten kunnen van nature goed tegen droogte, bijvoorbeeld omdat ze diepe of grote wortels hebben en daardoor goed water kunnen vinden. Maar dat zijn meestal niet de soorten die wij hier gebruiken als voedsel.';
         } else if (this.chosenIngredients.includes('D_Mulch')) {
-            this.message = 'Een laagje bladeren of stro helpt een beetje, maar dat zal jouw oogst niet redden!';
+            this.message = 'Een laagje bladeren of stro op de grond helpt om het water vast te houden en de bodem koel te houden. Maar bij lange periodes van droogte is dat niet genoeg: planten hebben dan extra hulp nodig, zoals aangepaste rassen of slimme technieken om water te voorzien.';
         } else if (this.chosenIngredients.includes('F_Chemical')) {
-            this.message = 'Dit middel helpt om water sneller af te voeren uit de bodem, maar door die chemische stoffen raakt jouw bodem uitgeput en ziek, wat niet goed is voor planten en dieren. ';
+            this.message = 'Dit middel helpt om water sneller af te voeren uit de bodem, maar door die chemische stoffen raakt jouw bodem uitgeput en ziek, wat schadelijk is voor planten en dieren.';
         } else if (this.chosenIngredients.includes('F_CRISPR')) {
-            this.message = 'Super! Dankzij CRISPR kunnen we planten verbeteren zonder schadelijke stoffen te gebruiken. Hierdoor kunnen we de planten beter tegen overstromingen beschermen!';
+            this.message = 'Super! Met CRISPR kunnen we planten sterker maken zonder chemische middelen te gebruiken. Zo kunnen we ze beter laten overleven bij overstromingen, bijvoorbeeld door ze te helpen tegen te veel water in de grond!';
         } else if (this.chosenIngredients.includes('F_Microbial')) {
-            this.message = 'Dit is een goede oplossing! Sommige bodembacteriën kunnen planten via de wortelen helpen om goed te groeien wanneer er teveel water is, helemaal natuurlijk!';
+            this.message = 'Dit is een slimme en natuurlijke oplossing! Sommige bodembacteriën helpen planten via hun wortels om beter te groeien, zelfs als de grond te nat is. Ze beschermen de wortels en helpen de plant hiermee om te gaan door meer zuurstof te voorzien rond de wortels.';
         } else if (this.chosenIngredients.includes('F_Raised_Soil')) {
-            this.message = 'Opgehoogde akkers helpen soms om de planten te beschermen van het water, maar werken niet altijd als het veel regent en kost veel werk.';
+            this.message = 'Opgehoogde akkers kunnen planten beschermen tegen teveel water, omdat ze iets hoger liggen dan de rest van het land. Maar als het heel veel regent werken ze niet altijd goed, en het kost veel tijd en moeite om ze aan te leggen.';
         } else if (this.chosenIngredients.includes('F_Wild_Relatives')) {
-            this.message = 'Sommige familieleden van jouw planten kunnen van nature goed tegen teveel water, maar dit zijn niet degene die wij hier groeien om op te eten.';
+            this.message = 'Sommige plantensoorten uit dezelfde familie van jouw planten kunnen van nature goed tegen te natte bodems, omdat ze bijvoorbeeld sterke wortels hebben of meer plek voor zuurstof voorzien. Maar dit zijn meestal niet degene die wij hier groeien om op te eten.';
         } else if (this.chosenIngredients.includes('F_Fertilizer')) {
-            this.message = 'Mest is goed voor planten, maar te veel ervan zal jouw grond en water vervuilen...';
+            this.message = 'Mest is goed voor planten, omdat het vol voedingsstoffen zit. Maar als je te veel gebruikt (vooral bij veel regen) spoelt het weg en vervuilt het de bodem en het water om ons heen.';
         } else if (this.chosenIngredients.includes('I_CRISPR')) {
-            this.message = 'Super! Dankzij CRISPR kunnen we planten verbeteren zonder schadelijke stoffen te gebruiken. Hierdoor kunnen we de planten beter tegen schadelijke insecten beschermen!';
+            this.message = 'Super! CRISPR is een speciale techniek waarmee wetenschappers het DNA van planten precies kunnen aanpassen. Met CRISPR kunnen we planten sterker maken, zodat ze beter tegen schadelijke insecten kunnen, zonder schadelijke stoffen te gebruiken!.';
         } else if (this.chosenIngredients.includes('I_Burning')) {
-            this.message = 'Je kan de zieke planten verbranden terwijl je de insecten wegjaagt, maar dan vervuil je de lucht en ben je waarschijnlijk ook een deel van jouw gezonde planten kwijt!';
+            this.message = 'Je kan de zieke planten verbranden om de insecten weg te krijgen, maar dan maak je schadelijke stoffen voor de natuur en ben je ook een deel van jouw gezonde planten kwijt!';
         } else if (this.chosenIngredients.includes('I_Insects')) {
-            this.message = 'Deze goede insecten zullen je helpen door de schadelijke insecten op te eten, maar dit lost vaak het probleem niet volledig op!';
+            this.message = 'Sommige insecten zijn heel nuttig: ze kunnen de rupsen opeten die je planten aantasten. Maar vaak is dit alleen niet genoeg om het probleem helemaal op te lossen!';
         } else if (this.chosenIngredients.includes('I_Microbial')) {
-            this.message = 'Dit werkt goed! Jouw planten zullen beschermd zijn tegen de insecten dankzij een natuurlijk middeltje van een bacterie dat rupsen weghoudt.';
+            this.message = 'Dit werkt goed! De planten worden beschermd tegen schadelijke insecten dankzij een natuurlijk middeltje dat afkomstig is van een bacterie. Deze bacterie maakt een stofje aan dat giftig is voor bepaalde insecten, zoals rupsen. Voor mensen, dieren en de planten zelf is dit veilig.';
         } else {
-            this.message = 'Sommige planten zoals lavendel, munt en basilicum kunnen van zichzelf insecten afschrikken met speciale geuren, dit zal natuurlijk niet alle rupsen wegjagen en als je deze ook in je akker zet heb je wel minder plaats voor jouw planten!';
+            this.message = 'Sommige planten zoals lavendel, munt en basilicum kunnen van zichzelf insecten afschrikken door hun sterke geur. Maar ze houden niet alle rupsen tegen en als je deze planten ook gebruikt heb je minder plaats voor jouw andere planten!';
         } 
         this.transitionMessage = dom.querySelector('h2');
         this.transitionMessage.textContent = this.message;
